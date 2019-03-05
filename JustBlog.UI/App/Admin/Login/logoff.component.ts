ab454d53fa4f675b19a0a0f3dd43e1c7b6d33c93
Brian Mikinski <brian.mikinski@gmail.com>
2019-03-04 18:36:06 -0600

    fix login html fix update login ux and html

--- JustBlog.UI/App/Admin/Login/logoff.component.ts
+++ JustBlog.UI/App/Admin/Login/logoff.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from "Core/auth.service";
 import { ComponentBase } from "Core/component.base";
 import { BaseController } from "Core/Models/BaseController";
 import { NotificationFactory } from "Notification/notification.factory";
+import { HomeComponentName } from "Blog/Home/home.component";
 
 export const LogoffComponentName: string = "logoff";
 
@@ -16,25 +17,24 @@ class LogoffComponentController extends BaseController implements ng.IController
                 public adminService: AdminService,
                 public notificationFactory: NotificationFactory,
                 public $sce: ng.ISCEService,
-                public $location: ng.ILocationService) {
+                public $state: ng.ui.IStateService) {
         super($sce);
     }
-
-    $onInit?(): void;
 
     /**
      * Log a user out of the admin section of the application
      */
     public Logoff(): void {
 
-        let onLogOffCallback: (response: boolean) => void;
-        onLogOffCallback = (response: boolean) => {
+        let onLogOffCallback: (response: boolean) => void = (response: boolean) => {
             let loginAttempt: boolean = <boolean>response;
             if (loginAttempt) {
+
                 this.notificationFactory.Success("Logoff was successful.");
 
                 // reroute to the management screen now that we have logged in
-                this.$location.path("/");
+                this.$state.go(HomeComponentName)
+
             } else {
                 // notify that we did 
                 this.notificationFactory.Error("Logoff was unsuccessful.");
@@ -56,10 +56,9 @@ export class LogoffComponent extends ComponentBase {
         this.bindings = {}
 
         this.controller = LogoffComponentController;
-        this.controllerAs = "$logoffCtrl"
 
         this.templateUrl = ["$element", "$attrs", ($element: ng.IAugmentedJQuery, $attrs: ng.IAttributes): string => {
-            return "Admin/Login/logoff.html"
+            return require("admin/login/logoff.html");
         }];
     }
 }
